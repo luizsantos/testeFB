971b1fbc01a79d07873ce26a641bbc6fe41ac926
luizsantos <luiz.arthur.feitosa.santos@gmail.com>
2012-04-08 00:13:52 -0300

    Update src/br/com/fb/controller/TokenFB.java

--- src/br/com/fb/controller/TokenFB.java
+++ src/br/com/fb/controller/TokenFB.java
@@ -3,11 +3,11 @@ package br.com.fb.controller;
 public class TokenFB {
 	// https://developers.facebook.com/tools/explorer?method=GET&path=1314662168
 	
-	private String Token = "AAACEdEose0cBAEbvMY3Y25MPSZA8SKm06ppC0ZBqxZAX9SB4B0eZAYi31lapfnIGrjCgZALANvB6m7E3LZCdy47VAp0v72zDr1V10sxN9yq7g0WcP5jfZBz";
+	private String Token = "SeuToken";
 
 	public String getToken() {
 
-		System.out.println("\nCaso não funcione lembre de ver se o Token não expirou!\n Veja também se o Token tem permissão de acessar os dados...");
+		System.out.println("\nCaso nï¿½o funcione lembre de ver se o Token nï¿½o expirou!\n Veja tambï¿½m se o Token tem permissï¿½o de acessar os dados...");
 		System.out.println("\n Para pegar ou atualizar o Token acesse:\nhttps://developers.facebook.com/tools/explorer?method=GET&path=1314662168");
 		
 		return Token;
